fd5d9bf3f46173cd1b3de6398a6af576f7179996
procesaur <31268468+procesaur@users.noreply.github.com>
2022-08-24 13:46:30 +0200

    ini

---
+++ main.py
@@ -0,0 +1,19 @@
+# Import important package
+import os
+import sys
+import spacy
+import glob
+import locale
+import urllib.request
+
+# for zip files
+import wget
+
+# xml
+from lxml import etree
+
+import pandas as pd
+
+# Import package for spaCy
+import spacy
+from spacy.tokens import Doc
