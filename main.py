c558b1891682f5d940d0c1c149e721eb02db9989
Mihailo Škorić <31268468+procesaur@users.noreply.github.com>
2022-08-25 14:59:58 +0200

    tmx++

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 import pandas as pd
 import spacy
 import flask
-from flask import request, Response, render_template
+from flask import request, Response, render_template, make_response
 from urllib.parse import unquote
 import pathlib
 import os
+from lxml import etree
+import numpy as np
 
 
 dir = str(pathlib.Path(__file__).parent.resolve())
 conf = pd.read_csv(dir + "/config/lng_config.csv", delimiter='\t').set_index('lng')
+conf = conf.replace({np.nan: None})
+languages = conf.axes[0].tolist()
+languages = [x for x in languages if conf.loc[x]['map_ner_types'] is not None]
 tags = ['PERS', 'LOC', 'ORG', 'DEMO', 'EVENT', 'WORK']
 nlps = {}
+dic_remove_tags = {}
+dic_tags = {}
+for lng in languages:
+    dic_remove_tags[lng] = conf.loc[lng]['remove_types'].split(',')
+    dic_tags[lng] = conf.loc[lng]['map_ner_types'].split(',')
 
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = False
 
 
-def process(req):
+def process_mono(req):
     query_parameters = req.form
-    lang = query_parameters.get('lng')
     if "file" in req.files:
         file = req.files["file"]
         if file.filename != "":
@@ -33,25 +42,50 @@ def process(req):
         data = query_parameters.get('data')
         data = unquote(data)
         name = "results"
+
+    lang = query_parameters.get('lng')
     return data, lang, name
+
+
+def process_tmx(req):
+    file = req.files["file"]
+    if file.filename != "":
+        name = file.filename
+    else:
+        name = "results"
+
+    return file, name
 
 
 @app.route('/')
 def home():
-    return render_template('ui.html')
+    return render_template('ui.html', data=languages)
+
+
+@app.route('/example.tmx')
+def example():
+    template = render_template('example.tmx')
+    response = make_response(template)
+    response.headers['Content-Type'] = 'application/xml'
+    return response
+
+
+@app.route('/4api')
+def api():
+    return render_template('api.html', data=request.root_url)
 
 
 @app.route('/mono', methods=['POST'])
 def mono():
-    data, lng, name = process(request)
+    data, lng, name = process_mono(request)
     return Response(monolingual_ner(data, lng), mimetype="text/plain",
                     headers={'Content-Disposition': 'attachment;filename=' + name + '.ner'})
 
 
 @app.route('/tmx', methods=['POST'])
 def tmxd():
-    data, lng, name = process(request)
-    return Response(monolingual_ner(data, lng), mimetype="text/plain",
+    file, name = process_tmx(request)
+    return Response(bilingual_ner(file), mimetype="text/plain",
                     headers={'Content-Disposition': 'attachment;filename=' + name + '.ner'})
 
 
@@ -64,7 +98,13 @@ def replace_string(old, new, start, end):
 
 
 # apply NER and annotate with NER tags
-def apply_NER_model_mono(text, nlp, lst_remove_tags):
+def apply_NER_model_mono(text, lng):
+    global nlps
+    if lng not in nlps:
+        nlps[lng] = load_model(lng)
+    nlp = nlps[lng]
+
+    lst_remove_tags = dic_remove_tags[lng]
     # apply NER model to input text
     doc = nlp(text)
     # the number of characters by which we move position of label
@@ -81,11 +121,13 @@ def apply_NER_model_mono(text, nlp, lst_remove_tags):
             # add two lenghts for labels and five for <></>
             up = int(2 * len(ent.label_) + 5)
             move_p += up
+    marked_text = map_tags(marked_text, lng)
     return marked_text
 
 
 # tagset harmonisation: mapping NER labels to same tagset
-def map_tags(marked_text, tags, lst_tags):
+def map_tags(marked_text, lng):
+    lst_tags = dic_tags[lng]
     i = 0
     f = marked_text
     for ne_tags in lst_tags:
@@ -110,18 +152,33 @@ def load_model(lng):
 
 
 def monolingual_ner(data, lng):
-    global nlps
-    global conf
-    if lng not in nlps:
-        nlps[lng] = load_model(lng)
-    nlp = nlps[lng]
-    lst_remove_tags = conf.loc[lng]['remove_types'].split(',')
-    lst_tags = conf.loc[lng]['map_ner_types'].split(',')
+    try:
+        return apply_NER_model_mono(data, lng)
+    except:
+        return "Submited string or file was not properly formatted :(("
 
-    text_ner = apply_NER_model_mono(data, nlp, lst_remove_tags)
-    harmonized_ner_text = map_tags(text_ner, tags, lst_tags)
 
-    return harmonized_ner_text
+def bilingual_ner(file):
+    try:
+        el = etree.parse(file)
+        tus = el.xpath("//*[local-name()='tu']")
+        for tu in tus:
+            tuvs = tu.xpath("*[local-name()='tuv']")
+            for tuv in tuvs:
+                lng = tuv.xpath("./@xml:lang", namespaces={'xml': 'http://www.w3.org/XML/1998/namespace'})[0]
+                data = tuv.getchildren()[0].text  # tuv/seg
+                # apply model and harmonise tagset
+                text_ner = apply_NER_model_mono(data, lng)
+                tuv.getchildren()[0].text = text_ner  # ažurirati tuv/seg sadržaj
+
+        # read xml as string
+        xml_str = etree.tostring(el, encoding='unicode')
+        # replace special characters
+        xml_str = xml_str.replace("&gt;", ">").replace("&lt;", "<")
+        return xml_str
+
+    except:
+        return "Submited file was not properly formatted :(("
 
 
 if __name__ == "__main__":
